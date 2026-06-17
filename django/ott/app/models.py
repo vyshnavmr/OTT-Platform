2617d381c2cce20dc4549b9cbb8213771c25fbc3
Vyshnav <vyshnav2004sk@gmail.com>
2026-06-17 11:07:37 +0530

    Created models.py: 4 models

--- django/ott/app/models.py
+++ django/ott/app/models.py
@@ -1,3 +1,46 @@
-from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager 
+from django.db import models 
 
-# Create your models here.
+
+class UserManager(BaseUserManager): 
+    def create_user(self, email, password=None):
+        if not email: 
+            raise ValueError("Users must have an email address") 
+        email = self.normalize_email(email) 
+        user = self.model(email=email) 
+        user.set_password(password) 
+        user.save(using=self._db) 
+        return user 
+
+    def create_superuser(self, email, password): 
+        user = self.create_user(email, password) 
+        user.is_admin = True 
+
+        User.is_superuser = True 
+        user.save(using=self._db) 
+        return user 
+    
+class User(AbstractBaseUser): 
+        email = models.EmailField(unique=True) 
+        name = models.CharField(max_length =255) 
+        is_active = models.BooleanField(default=True) 
+        is_admin = models.BooleanField(default=False) 
+        objects = UserManager() 
+        
+        USERNAME_FIELD = 'email'
+
+class Movie(models.Model):
+    name = models.CharField(max_length=255)
+    thumbnail = models.FileField()
+    description = models.CharField(max_length=255)
+    video = models.FileField()
+    views = models.IntegerField(default=0)
+
+class WatchList(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class WachHistory(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE) 
+    watched_at = models.DateTimeField(auto_now_add=True)
--- django/ott/ott/settings.py
+++ django/ott/ott/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'ott.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ott_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',  # or server IP
+        'PORT': '3306',
     }
 }
 
@@ -122,3 +126,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'app.User'
