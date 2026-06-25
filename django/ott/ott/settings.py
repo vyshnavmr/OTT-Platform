82ee76462a884c862fcd4bf86040d9f4ddcdcf97
Vyshnav <vyshnav2004sk@gmail.com>
2026-06-25 16:39:47 +0530

    Edits in settings

--- django/ott/ott/settings.py
+++ django/ott/ott/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bmm2q7s%#@d6tl6fc-u5lixrns(^&q95!m6v=#tb^qw9*kfun)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -140,6 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
