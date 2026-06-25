f1a2169b8958bea4686fff6b13f896c3fa300730
Vyshnav <vyshnav2004sk@gmail.com>
2026-06-25 12:26:17 +0530

    added logout view

--- django/ott/app/urls.py
+++ django/ott/app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path('',views.adminlogin),
+    path('',views.adminlogin, name='adminlogin'),
     path('home/',views.home, name='home'),
     path('block-user/<int:user_id>/', views.blockuser, name='blockuser'),
     path('unblock-user/<int:user_id>/', views.unblockuser, name='unblockuser'),
@@ -12,7 +12,8 @@ urlpatterns = [
     path('movielist/',views.movielist, name='movielist'),
     path('editmovie/<int:movie_id>/',views.edit_movie,name='editmovie'),
     path('history/<int:user_id>/',views.user_history,name='user_history'),
-    path('reports/', views.reports, name='reports')
+    path('reports/', views.reports, name='reports'),
+    path("logout/", views.adminlogout, name="logout"),
 ]
 
 
